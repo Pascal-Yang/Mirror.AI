852dfc7d20902ee8d7aa661bd3abcc9d76aa4291
Elyse-Tang <tang.z@wustl.edu>
2023-03-19 16:02:45 -0500

    added companies

--- Mirror/Mirror/Models/Company.swift
+++ Mirror/Mirror/Models/Company.swift
@@ -17,4 +17,8 @@ struct Company: Hashable {
 
 struct Companies {
     static let Google = Company(name: "Google", logo: "google_logo", description: "Google is an American multinational technology company that specializes in internet-related services and products.")
+    static let Amazon = Company(name: "Amazon", logo: "amazon_logo", description: "Amazon is a multinational tech company known for its e-commerce platform, cloud computing services, digital streaming, and artificial intelligence products.")
+    static let Meta = Company(name: "Meta", logo: "meta_logo", description: "Meta is a tech company that provides social networking and digital communication services.")
+    static let Microsoft = Company(name: "Microsoft", logo: "microsoft_logo", description: "Microsoft is a technology company that offers software, hardware, and cloud services.")
+
 }

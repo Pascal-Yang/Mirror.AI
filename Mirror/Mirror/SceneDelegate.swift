b07cf393f8a46c496b8e6dacd7ece61942947a64
jeffrey <jeffrey.w@wustl.edu>
2023-03-26 17:03:46 -0500

    continue as guest

--- Mirror/Mirror/SceneDelegate.swift
+++ Mirror/Mirror/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let loginView = LoginPage()
         //        let chatView = ChatView()
-        let DashboardView = DashboardView(selectedCompany: Companies.Google)
+        let DashboardView = DashboardView(selectedCompany: Companies.General)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {

f4f47771ee1b2e0358df81770a3d7b51b72bd5f3
jeffrey <jeffrey.w@wustl.edu>
2023-04-06 21:46:06 -0500

    chatHelper sendMessage function updated

--- Mirror/Mirror/Helper/ChatHelper.swift
+++ Mirror/Mirror/Helper/ChatHelper.swift
@@ -13,17 +13,64 @@ class ChatHelper : ObservableObject {
         
         realTimeMessages.append(chatMessage)
         didChange.send(())
+        var score = "0";
         
-        //If the response from the fetchData function is not nil,
+        print("evaluation process:")
+        var pendingMessage = chatMessage.content
+        print(pendingMessage)
         
-        if let res = fetchCompletion(prompt: chatMessage.content){
+        //If pendingMessage is one of the preset message of msgbtnsview, don't evaluate it
+        if pendingMessage == "give me a hint to the interview question you just asked, keep it relatively short" ||
+           pendingMessage == "give me a standard answer to the interview question you just asked" ||
+           pendingMessage == "give me another question" {
             
-            // Create a new chat message from the response and append it to the realTimeMessages data source
-            let new = Message(content: res, user: DataSource.firstUser, fromAPI:true)
-            realTimeMessages.append(new)
-            didChange.send(())
+            if let res = fetchCompletion(prompt: pendingMessage){
+                // Create a new chat message from the response and append it to the realTimeMessages data source
+                let new = Message(content: res, user: DataSource.firstUser, fromAPI:true)
+                realTimeMessages.append(new)
+                didChange.send(())
+            }
             
         }
+        //If pendingMessage is a user response, evaluate it
+        else{
+            let evaluatePrompt = "Evaluate this answer to your question like a real interviewer and give me a score out of 10, along with 2 short advices:'" + String(pendingMessage) + "'."
+            print(evaluatePrompt)
+
+            if let res = fetchCompletion(prompt: evaluatePrompt){
+                // Create a new chat message from the response and append it to the realTimeMessages data source
+                print(res)
+                
+                var charBefore: Character = "0"
+                // Extract the score from res
+                if let range = res.range(of: "/10").map({ $0.lowerBound }) ?? res.range(of: " out of 10").map({ $0.lowerBound }), range != res.startIndex {
+                    charBefore = res[res.index(before: range)]
+                }
+                
+                if charBefore == "0" && res.index(before: res.endIndex) != res.startIndex {
+                    let charBeforeZero = res[res.index(before: res.index(before: res.endIndex))]
+                    if charBeforeZero == "1" {
+                        let extractedScore = "10"
+                        print("Extracted score:", extractedScore)
+                        score = extractedScore
+                    }
+                } else {
+                    let extractedScore = String(charBefore)
+                    print("Extracted score:", extractedScore)
+                    score = extractedScore
+                }
+                
+                let new = Message(content: res, user: DataSource.firstUser, fromAPI:true)
+                realTimeMessages.append(new)
+                didChange.send(())
+            }
+        }
+        
+        if Int(score) == nil {
+            let defaultScore = "0"
+            print("Score is not a numeric string, setting it to the default value:", defaultScore)
+        }
+        print("final score:", score)
     }
     
     func configureChatroom(_ param: [String:String]) {
